9b357b35118d7a47958053534c2cb144f783fc24
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2017-08-24 19:03:55 +0530

    final

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
    setTimeout(function(){
         $("#preloader").fadeOut(2000);
@@ -15,21 +14,4 @@ $(document).ready(function(){
         $("#header .srchbar").slideToggle(1000);
     });
     $.fatNav();
-=======
-$(document).ready(function(){
-   setTimeout(function(){
-        $("#preloader").fadeOut(2000);
-    },4000);
-   var owl = $('.owl-carousel'); 
-   owl.owlCarousel({
-      items:1,
-      loop:true,
-      autoplay:true,
-      autoplayTimeout:3000,
-    });
-    new WOW().init();
-    $("#srch").click(function(){
-        $("#header .srchbar").slideToggle(1000);
-    });
->>>>>>> origin/master
 });
